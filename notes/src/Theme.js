ab59d8877b2e35e2b4780353ae56ac530df284bb
limjingyun <limjingyun@gmail.com>
2023-05-13 17:13:28 +0800

    helpplz

--- notes/src/Theme.js
+++ notes/src/Theme.js
@@ -2,8 +2,11 @@ import { createTheme } from '@mui/material'
 
 const Theme = createTheme({
     palette: {
-        primary: '#D8FFEF',
-        secondary: '#F1E8FF',
+
+    },
+
+    typography: {
+
     }
 });
 
