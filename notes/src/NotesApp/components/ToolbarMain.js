90228abb75b90fe32ae345c6033ffcded8168e03
yuxunn <teeyuxun@gmail.com>
2023-06-22 16:53:39 +0800

    font still doesnt change

--- notes/src/NotesApp/components/ToolbarMain.js
+++ notes/src/NotesApp/components/ToolbarMain.js
@@ -9,24 +9,37 @@ const ToolbarMain = ({ value, onChange }) => {
     onChange(content);
   };
 
+  const handleFontChange = (e) => {
+    const font = e.target.value;
+    editorRef.current.getEditor().format('font', font);
+  };
 
-  const formats = [
+  const fonts = [
+    { label: 'Arial', value: 'Arial, sans-serif' },
+    { label: 'Times New Roman', value: 'Times New Roman, serif' },
+    { label: 'Courier New', value: 'Courier New, monospace' },
   ];
+
+  const toolbarOptions = {
+    toolbar: {
+      container: [
+        ['bold', 'italic', 'underline', 'strike'],
+        [{ font: fonts.map((font) => font.value) }], 
+        ['link', 'image', 'video'], 
+      ],
+    },
+  };
 
   return (
     <div className="text-editor-toolbar">
-      <div className="text-editor-toolbar-row">
-        <div className="text-editor-toolbar-buttons">
-        </div>
-      </div>
-      <ReactQuill 
-      ref={editorRef} 
-      value={value} 
-      onChange={handleEditorChange}
-       />
+      <ReactQuill
+        ref={editorRef}
+        value={value}
+        onChange={handleEditorChange}
+        modules={toolbarOptions}
+      />
     </div>
   );
 };
 
 export default ToolbarMain;
-
